242e122ec565071d7dbe7b40cd9bfa70d5c4415f
zhaoyingjun <>
2020-01-31 21:16:17 +0800

    first commit

--- core-site/core.py
+++ core-site/core.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2020-01-31 20:54
 # @Author  : Enjoy Zhao
+class GrEexception(Exception):
+    """
+
+    """
+
+class Client:
+    """
+
+
+    """
+    def save(self,filename,overwrite=False):
+        """
+        """
+        raise NotImplementedError()
+
